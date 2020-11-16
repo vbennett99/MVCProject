9b11f44b1ae6f8ae2f9d298dcf069b93859a9733
Vincent Bennett <vmbennett99@gmail.com>
2020-11-15 21:43:47 -0500

    Fixed(?) upload, messed up profile lol

--- client/app/profile.js
+++ client/app/profile.js
@@ -1,8 +1,29 @@
 const handleProfile = (e) => {
   e.preventDefault();
-  
   $("#popupMessage").animate({width:'hide'}, 350);
+  return false;
 };
+
+//const PieceForm = (props) => {
+//  return (
+//    <form id="pieceForm"
+//          onSubmit={HandleProfile}
+//          name="pieceForm"
+//          action="/upload"
+//          method="POST"
+//          className="pieceForm"
+//    >
+//      <label htmlFor="Title">Title: </label>
+//      <input id="pieceTitle" type="text" name="title" placeholder="Piece title"/>
+//      <label htmlFor="Tags">Tags: </label>
+//      <input id="pieceTags" type="text" name="tags" placeholder="Romance, Comedy, ..."/>
+//      <label htmlFor="body">Body: </label>
+//      <input id="pieceBody" type="text" name="body" placeholder="Your writing goes here"/>
+//      <input type="hidden" name="_csrf" value={props.csrf}/>
+//      <input className="uploadPieceSubmit" type="submit" value="Upload Piece"/>
+//    </form>
+//  );
+//};
 
 const ProfileInfo = (props) => {
   return (
@@ -47,14 +68,37 @@ const loadPiecesFromServer = () => {
   });
 };
 
-const setup = function(csrf){
+//Rendering Functions
+const createProfileInfo = (csrf) => {
   ReactDOM.render(
     <ProfileInfo />, document.querySelector("#profileInfo")
   );
-  
+};
+
+const createPieceList = (csrf) => {
   ReactDOM.render(
-    <PieceList pieces={[]} />, document.querySelector("#piecePreviews")
+    <PieceList pieces={data.pieces} />, document.querySelector("#piecePreviews")
   );
+};
+
+//const createPieceForm = (csrf) => {
+//  ReactDOM.render(
+//    <PieceForm csrf={csrf} />, document.querySelector("#piecePreviews")
+//  );
+//};
+
+const setup = (csrf) => {
+  //Default view items
+  createProfileInfo(csrf);
+  createPieceList(csrf);
+  
+//  //If the upload button is clicked, switch from piece previews to upload form
+//  const uploadButton = document.querySelector("#uploadNewButton");
+//  uploadButton.addEventListener("click", (e) => {
+//    e.preventDefault();
+//    createPieceForm(csrf);
+//    return false;
+//  });
   
   loadPiecesFromServer();
 };
--- hosted/bundle.js
+++ hosted/bundle.js
@@ -5,7 +5,28 @@ var handleProfile = function handleProfile(e) {
   $("#popupMessage").animate({
     width: 'hide'
   }, 350);
-};
+  return false;
+}; //const PieceForm = (props) => {
+//  return (
+//    <form id="pieceForm"
+//          onSubmit={HandleProfile}
+//          name="pieceForm"
+//          action="/upload"
+//          method="POST"
+//          className="pieceForm"
+//    >
+//      <label htmlFor="Title">Title: </label>
+//      <input id="pieceTitle" type="text" name="title" placeholder="Piece title"/>
+//      <label htmlFor="Tags">Tags: </label>
+//      <input id="pieceTags" type="text" name="tags" placeholder="Romance, Comedy, ..."/>
+//      <label htmlFor="body">Body: </label>
+//      <input id="pieceBody" type="text" name="body" placeholder="Your writing goes here"/>
+//      <input type="hidden" name="_csrf" value={props.csrf}/>
+//      <input className="uploadPieceSubmit" type="submit" value="Upload Piece"/>
+//    </form>
+//  );
+//};
+
 
 var ProfileInfo = function ProfileInfo(props) {
   return /*#__PURE__*/React.createElement("div", {
@@ -49,13 +70,35 @@ var loadPiecesFromServer = function loadPiecesFromServer() {
       pieces: data.pieces
     }), document.querySelector("#piecePreviews"));
   });
+}; //Rendering Functions
+
+
+var createProfileInfo = function createProfileInfo(csrf) {
+  ReactDOM.render( /*#__PURE__*/React.createElement(ProfileInfo, null), document.querySelector("#profileInfo"));
 };
 
-var setup = function setup(csrf) {
-  ReactDOM.render( /*#__PURE__*/React.createElement(ProfileInfo, null), document.querySelector("#profileInfo"));
+var createPieceList = function createPieceList(csrf) {
   ReactDOM.render( /*#__PURE__*/React.createElement(PieceList, {
-    pieces: []
+    pieces: data.pieces
   }), document.querySelector("#piecePreviews"));
+}; //const createPieceForm = (csrf) => {
+//  ReactDOM.render(
+//    <PieceForm csrf={csrf} />, document.querySelector("#piecePreviews")
+//  );
+//};
+
+
+var setup = function setup(csrf) {
+  //Default view items
+  createProfileInfo(csrf);
+  createPieceList(csrf); //  //If the upload button is clicked, switch from piece previews to upload form
+  //  const uploadButton = document.querySelector("#uploadNewButton");
+  //  uploadButton.addEventListener("click", (e) => {
+  //    e.preventDefault();
+  //    createPieceForm(csrf);
+  //    return false;
+  //  });
+
   loadPiecesFromServer();
 };
 
